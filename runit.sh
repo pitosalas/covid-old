a9d8941a94739ec4a542c1c30d96c3d13bbbd067
Pito Salas <pitosalas@gmail.com>
2020-05-14 08:10:22 -0400

    all

--- runit.sh
+++ runit.sh
@@ -1,6 +1,6 @@
 echo "[Running covid reports]"
-python3 covid.py graph1 --states "USA"  "New York" --vars "casesc"  "deathsc"
-python3 covid.py graph2 --states "USA"  "New York" --vars "casesr"  "deathsr"
-python3 covid.py graph3 --states "Massachusetts" "Florida" "California"  "Washington" --vars "casesc"  "deathsc"
-python3 covid.py graph4 --states "Massachusetts" "Florida" "California"  "Washington" --vars "casesr"  "deathsr"
-python3 covid.py graph5 --states "Massachusetts" "Florida" "California"  "Washington" --vars "casesd"  "deathsd"
+python3 covid.py graph1 --states "USA"  "New York" "Massachusetts" --vars "casesr"  "deathsr"
+python3 covid.py graph2 --states "Florida" "California"  "District of Columbia" "USA"  "New York" "Massachusetts" "South Carolina" "New Hampshire"  "Washington" --vars "casesr"  "deathsr"
+python3 covid.py graph3 --states "Massachusetts" "Florida" "California"  "District of Columbia" --vars "casesr"  "deathsr"
+python3 covid.py graph4 --states "South Carolina" "New Hampshire"  "Washington" --vars "casesr"  "deathsr"
+
