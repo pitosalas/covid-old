82f8ff172d8508abde9eb6e7673a936370ce3969
Pito Salas <pitosalas@gmail.com>
2020-05-16 11:28:56 -0400

    tiny

--- runit.sh
+++ runit.sh
@@ -2,6 +2,6 @@ echo "[Running covid reports]"
 
 python3 covid.py graph1 --states USA  "New York"  --vars casesr  deathsr
 python3 covid.py graph2 --states Massachusetts "New Jersey" "New York" --vars casesr  deathsr
-python3 covid.py graph3 --states  "District of Columbia" Florida --vars casesr  deathsr
-python3 covid.py graph4 --states "South Carolina" "West Virginia" Texas --vars casesr  deathsr
+python3 covid.py graph3 --states Texas Florida --vars casesr  deathsr
+python3 covid.py graph4 --states "District of Columbia" "South Carolina" "West Virginia" Texas --vars casesr  deathsr
 python3 covid.py graph5 --states California Washington --vars casesr  deathsr
