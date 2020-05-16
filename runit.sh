84877410dd9da57dee83d8658cad086b28faa293
pitosalas <pitosalas@users.noreply.github.com>
2020-05-16 15:26:33 +0000

    deploy: 2d58a27e086eb9cbe56747aa989292bfad46f973

--- runit.sh
+++ runit.sh
@@ -1,6 +1,7 @@
 echo "[Running covid reports]"
 
 python3 covid.py graph1 --states USA  "New York"  --vars casesr  deathsr
-python3 covid.py graph2 --states Massachusetts "District of Columbia" Florida --vars casesr  deathsr
-python3 covid.py graph3 --states "South Carolina" "West Virginia" Texas --vars casesr  deathsr
-python3 covid.py graph4 --states California Washington --vars casesr  deathsr
+python3 covid.py graph2 --states Massachusetts "New Jersey" "New York" --vars casesr  deathsr
+python3 covid.py graph3 --states  "District of Columbia" Florida --vars casesr  deathsr
+python3 covid.py graph4 --states "South Carolina" "West Virginia" Texas --vars casesr  deathsr
+python3 covid.py graph5 --states California Washington --vars casesr  deathsr
