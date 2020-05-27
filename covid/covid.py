0a1a1b8acc12bc0c68340114c27b64d469d89aa4
pitosalas <pitosalas@users.noreply.github.com>
2020-05-27 19:35:03 +0000

    deploy: 488bfe1c345472d6a013ed14f8ffca57591684f8

--- covid/covid.py
+++ covid/covid.py
@@ -2,52 +2,74 @@ import data
 import graph
 import argparse
 import sys
+import pandas as pd
+
+
+def run_from_ipython():
+    try:
+        __IPYTHON__
+        return True
+    except NameError:
+        return False
+
 
 def command_parser():
     parser = argparse.ArgumentParser(description='Generate COVID graphs')
-    parser.add_argument("filename", action="store")
     parser.add_argument("--states", nargs="+", type=str)
-    parser.add_argument("--vars", nargs="+", type=str)
-    print(sys.argv)
-    if len(sys.argv) == 1:
-        args = parser.parse_args('testout --states ak al --vars  excessl deathsr'.split())
+    parser.add_argument("-v", "--vars", nargs="+", type=str)
+    parser.add_argument("-g", "--graph", nargs="+", type=str)
+    parser.add_argument("-d", "--data", nargs="+", type=str)
+    if len(sys.argv) == 1 or run_from_ipython():
+        args = parser.parse_args(
+            ' --data x --graph y --states ak al ma --vars  excessl deathsr'.split())
     else:
         args = parser.parse_args()
     return args
 
+
 def print_spec(states, variables, date, filename, dimensions):
     states_s = ', '.join(str(x) for x in states)
     variables_s = ', '.join(str(x) for x in variables)
-    print("Graph start date: ", date, "- for ", states_s, "- showing ", variables_s)
+    print("Graph start date: ", date, "- for ",
+          states_s, "- showing ", variables_s)
 
-def report_row(df, states, variables, date, filename, dimensions):
-    print_spec(states, variables, date, filename, dimensions)
-    graph.graph_b(df, states, variables, filename, dimensions)
 
 def doit():
-    statesmap = {"nc" : "North Carolina",
-                "wv" : "West Virginia",
-                "sc" : "South Carolina",
-                "dc" : "District of Columbia",
-                "ma" : "Massachusetts",
-                "nh" : "New Hampshire", 
-                "ny" : "New York",
-                "wa" : "Washington",
-                "nj": "New Jersey",
-                "ca" : "California",
-                "tx" : "Texas",
-                "fl" : "Florida",
-                "usa" : "USA",
-                "al" : "Alabama",
-                "ak" : "Arkansas"
-                }
+    statesmap = {"nc": "North Carolina",
+                 "wv": "West Virginia",
+                 "sc": "South Carolina",
+                 "dc": "District of Columbia",
+                 "ma": "Massachusetts",
+                 "nh": "New Hampshire",
+                 "ny": "New York",
+                 "wa": "Washington",
+                 "nj": "New Jersey",
+                 "ca": "California",
+                 "tx": "Texas",
+                 "fl": "Florida",
+                 "usa": "USA",
+                 "ct": "Connecticut",
+                 "al": "Alabama",
+                 "ak": "Arkansas",
+                 "vt": "Vermont"
+                 }
     args = command_parser()
     states = list(map(lambda state: statesmap[state], args.states))
     variables = args.vars
+    datfilename = args.data[0] + ".csv"
     startdate = "2020-03-01"
     dim = [4, 2.5]
-    df = data.read_data(startdate, states, variables)
-    print_spec(states, variables, startdate, args.filename, dim)
-    graph.graph_b(df, states, variables, args.filename, dim)
+    if args.data and not args.graph:
+        print("covid: saving data in " + datfilename)
+        df = data.read_data(startdate, states, variables)
+        df.to_csv(datfilename)
+    if args.graph:
+        graphfilename = args.graph[0]
+        print("covid: generating graph from " +
+              datfilename + " into " + graphfilename)
+        df = (pd.read_csv(datfilename, parse_dates=['date'])
+              .query("state in @states"))
+        graph.graph_b(df, states, variables, graphfilename, dim)
+
 
 doit()
--- covid/graph.py
+++ covid/graph.py
@@ -7,26 +7,26 @@ import seaborn as sns
 def graph_b(df, states, variables, filename, ratio):
     sns.set()
     plt.style.use('seaborn-darkgrid')
-    plt.legend(fontsize='xx-large')
-    g = sns.FacetGrid(df, col="variable", hue='state', sharex=True, col_order=variables, sharey=False, height=ratio[0], aspect=ratio[1])
+    g = sns.FacetGrid(df, col="variable", hue='state', sharex=True,
+                      col_order=variables, sharey=False, height=ratio[0], aspect=ratio[1])
     g = g.map(plt.plot, "date", "value")
     g.add_legend()
-    labelmap = {"deathsd": "Deaths Doubling", 
+    labelmap = {"deathsd": "Deaths Doubling",
                 "deaths": "Deaths",
-                "cases": "Cases", 
+                "cases": "Cases",
                 "casesd": "Cases Doubling",
-                "casesc": "New Cases", 
+                "casesc": "New Cases",
                 "deathsc": "New Deaths",
                 "excessl": "Excess Deaths",
-                "excessh" : "Excess Deaths (h)",
+                "excessh": "Excess Deaths (h)",
                 "deathsr": "New Deaths (rolling average)",
                 "casesr": "New Cases (rolling average)"}
     for i in range(len(variables)):
-        g.axes[0,i].set_title(labelmap[variables[i]])
+        g.axes[0, i].set_title(labelmap[variables[i]])
     xformatter = mdates.DateFormatter("%m/%d")
-    xlocator = mdates.DayLocator(bymonthday=[1,5,10, 15, 20, 25])
+    xlocator = mdates.DayLocator(bymonthday=[1, 5, 10, 15, 20, 25])
     yformatter = ticker.FuncFormatter(lambda x, p: format(int(x), ','))
-    g.axes[0,0].xaxis.set_major_formatter(xformatter)
-    g.axes[0,0].xaxis.set_major_locator(xlocator)
+    [axis[0].xaxis.set_major_formatter(xformatter) for axis in g.axes]
+    [axis[0].xaxis.set_major_locator(xlocator) for axis in g.axes]
     [axis[0].yaxis.set_major_formatter(yformatter) for axis in g.axes]
     plt.savefig(filename)
